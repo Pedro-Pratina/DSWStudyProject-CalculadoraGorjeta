f409c8d61a4cfb66bcc43a399c87789c438aa402
Pedro Henrique Reis Lima <160922340+Pedro-Pratina@users.noreply.github.com>
2024-03-31 00:59:38 +0000

    Ainda não entendi a logica de calculo

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
+    calcular()
 }
 
 const pessoasInput = document.querySelector("#pessoas")
 pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
 function receberQuantidadePessoas(evento) {
-    const divErro = document.querySelector(".pessoas .input-box")
     const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
 
     if(evento.target.value === "0") {
         paragrafoErro.style.display = "block"
@@ -24,6 +25,8 @@ function receberQuantidadePessoas(evento) {
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+
+    calcular()
 }
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
@@ -46,9 +49,36 @@ function receberPorcentagem(evento) {
         porcentagem = 0
     }
 
-    console.log(porcentagem)
+    calcular()
 }
 
 const gorjetaInput =  document.querySelector("#outra")
 gorjetaInput.addEventListener("input", receberPorcentagem)
 
+function calcular() {
+    if(conta !== 0 && porcentagem !== 0 && pessoas !== 0) {
+        const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
+        strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
+
+        const strongTotal = document.querySelector(".total > strong")
+        strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
+    }
+}
+
+const botaoLimpar = document.querySelector(".resultados button")
+botaoLimpar.addEventListener ("click", limpar)
+
+function limpar() {
+    contaInput.value = ""
+
+    botoesGorjeta.forEach (botao => {
+        botao.classList.remove("botao-ativa")
+    })
+
+    gorjetaInput.value = ""
+
+    pessoasInput.value = ""
+
+    document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+}
